1a871a3b4312fc970ed80438c94b0a73b2ea165a
w2lody <44412859+w2lody@users.noreply.github.com>
2018-12-07 22:53:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -493,4 +493,17 @@ if (message.content.startsWith(PREFIX + 'setavatar')) {
    message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
 }
 });
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : Diamond Codes`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : Diamond Codes ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(` 1play | Maltesers :musical_score:.`,"http://twitch.tv/S-F")
+client.user.setStatus("online")
+ 
+});
+
 client.login(process.env.BOT_TOKEN);
